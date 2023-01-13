4a273a5ae875dc3594016a6ca5fc918fa480a849
Huiwen-UQ <huiwen.zheng@uq.net.au>
2023-01-13 16:05:48 +1000

    Update plotPways.R

--- R/plotPways.R
+++ R/plotPways.R
@@ -2,12 +2,14 @@
 #' @author Huiwen Zheng
 #' @param pways_df Output from pathway analysis.
 #' @param n Number of pathways plotted in the barplot.
+#' @param p.adj Adjusted p value to select significantly enriched genes
 #' @export
-plotPways <- function(pways_df, n=10){
+plotPways <- function(pways_df, n=10, p.adj = 0.1){
   pways_df <- pways_df[order(pways_df$padj),]
   if (nrow(pways_df)>=n){n=n}
   if (nrow(pways_df)<n){n=nrow(pways_df)}
   pways_sub <- pways_df[1:n,]
+  pways_sub <- pways_sub[pways_sub$padj<p.adj,]
   #pways_sub$size <- as.numeric(pways_sub$size)
   pways_sub <- pways_sub[order(pways_sub$size, decreasing = F),]
   ggplot2::ggplot(pways_sub,aes(reorder(pathway, size), y= size, fill=padj))+
