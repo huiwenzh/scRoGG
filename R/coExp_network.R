05d4ee97f86352f1c2121178651e2a5f8bfa90ac
Huiwen Zheng <46465953+Huiwen-UQ@users.noreply.github.com>
2023-01-27 10:20:09 +1000

    Update coExp_network.R

--- R/coExp_network.R
+++ R/coExp_network.R
@@ -28,28 +28,23 @@ coExp_network <- function(edge_dat,min_size=1,n_networks=4){
   for (n in 1:n_networks){
     selnodes <- igraph::V(G_test)[c1$membership==names(table(c1$membership)[order(table(c1$membership),decreasing = T)])[n]]
     selegoG <- igraph::induced_subgraph(G_test,selnodes)
-    igraph::edge_density(selegoG, loops=F)
+
     selegoG1 <- selegoG
     igraph::E(selegoG1)$weight <- abs(igraph::E(selegoG)$weight)
+    igraph::E(selegoG)$color <- ifelse(igraph::E(selegoG)$weight > 0,'#00D100','#B30000')
     l <- igraph::layout.fruchterman.reingold(selegoG1)
     plot(selegoG, layout=l,    # === vertex
-         vertex.color =rgb(0.6,0.6,0.6,0.6),          # Node color
+         vertex.color = rgb(0.6,0.6,0.6,0.6),          # Node color
          vertex.frame.color = "white",                 # Node border color
-         vertex.shape="circle",                        # One of “none”, “circle”, “square”, “csquare”, “rectangle” “crectangle”, “vrectangle”, “pie”, “raster”, or “sphere”
          vertex.size=13,                               # Size of the node (default is 15)
-         vertex.size2=NA,                              # The second size of the node (e.g. for a rectangle)
          # Character vector used to label the nodes
          vertex.label.color="black",
-
-         vertex.label.font=1,                          # Font: 1 plain, 2 bold, 3, italic, 4 bold italic, 5 symbol
          vertex.label.cex=0.7,                           # Font size (multiplication factor, device-dependent)
          vertex.label.dist=0,                          # Distance between the label and the vertex
          vertex.label.degree=0 ,                       # The position of the label in relation to the vertex (use pi)
-
-         # === Edge
-         edge.color="#cc6600",                           # Edge color
          edge.width=igraph::E(selegoG)$weight,                                 # Edge width, defaults to 1
          edge.arrow.size=1,                            # Arrow size, defaults to 1
+         edge.color=igraph::E(selegoG)$color,
          edge.lty=1,
          edge.curved=0  ,                          # Edge curvature, range 0-1 (FALSE sets it to 0, TRUE to 0.5)
     )
